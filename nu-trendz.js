e5af785b198b894d31c6dd4983d6fa0242f24744
johntmachangara-eng <johntmachangara@gmail.com>
2025-10-21 23:13:43 +0100

    Add files via upload

--- nu-trendz.js
+++ nu-trendz.js
@@ -1,11 +1,2 @@
-const services = document.querySelectorAll('.single-service');
 
-services.forEach(service => {
-service.addEventListener('click', () => {
-const overlay = service.querySelector('.overlay');
-const desc = service.querySelector('.portfolio-desc');
-overlay.style.opacity = overlay.style.opacity === '1' ? '0' : '1';
-desc.style.opacity = desc.style.opacity === '1' ? '0' : '1';
-});
-});
 
